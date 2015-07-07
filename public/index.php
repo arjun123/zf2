a0d037a281fe0966def5ffd03f43176d5890ef18
arjun <arjunsunar@lftechnology.com>
2015-07-07 16:37:36 +0545

    album is added

--- public/index.php
+++ public/index.php
@@ -3,10 +3,12 @@
  * This makes our life easier when dealing with paths. Everything is relative
  * to the application root now.
  */
+@ini_set('display_errors', 1);
+error_reporting(E_ALL);
 chdir(dirname(__DIR__));
 
 // Setup autoloading
-include 'init_autoloader.php';
-
+require 'init_autoloader.php';
+// csds
 // Run the application!
-Zend\Mvc\Application::init(include 'config/application.config.php')->run()->send();
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
