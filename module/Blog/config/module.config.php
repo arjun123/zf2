237191e310409ba4a104599d119311fe9feb3f6a
arjun <arjunsunar@lftechnology.com>
2015-08-01 00:12:32 +0545

    Service and Factory class are used for list post

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -1,8 +1,19 @@
 <?php
 return array(
+    'db' => array(
+         'driver'         => 'Pdo',
+         'username'       => 'root',  //edit this
+         'password'       => 'password',  //edit this
+         'dsn'            => 'mysql:dbname=blog;host=localhost',
+         'driver_options' => array(
+             \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
+         )
+     ),
     'service_manager' => array(
-         'invokables' => array(
-             'Blog\Service\PostServiceInterface' => 'Blog\Service\PostService'
+         'factories' => array(
+             'Blog\Mapper\PostMapperInterface'   => 'Blog\Factory\ZendDbSqlMapperFactory',
+             'Blog\Service\PostServiceInterface' => 'Blog\Factory\PostServiceFactory',
+             'Zend\Db\Adapter\Adapter'           => 'Zend\Db\Adapter\AdapterServiceFactory'
          )
      ),
     'view_manager' => array(
@@ -17,20 +28,29 @@ return array(
       ),
      // This lines opens the configuration for the RouteManager
      'router' => array(
-         // Open configuration for all possible routes
          'routes' => array(
-             // Define a new route called "post"
-             'post' => array(
-                 // Define the routes type to be "Zend\Mvc\Router\Http\Literal", which is basically just a string
+             'blog' => array(
                  'type' => 'literal',
-                 // Configure the route itself
                  'options' => array(
-                     // Listen to "/blog" as uri
                      'route'    => '/blog',
-                     // Define default controller and action to be called when this route is matched
                      'defaults' => array(
                          'controller' => 'Blog\Controller\List',
                          'action'     => 'index',
+                     ),
+                 ),
+                 'may_terminate' => true,
+                 'child_routes'  => array(
+                     'detail' => array(
+                         'type' => 'segment',
+                         'options' => array(
+                             'route'    => '/:id',
+                             'defaults' => array(
+                                 'action' => 'detail'
+                             ),
+                             'constraints' => array(
+                                 'id' => '[1-9]\d*'
+                             )
+                         )
                      )
                  )
              )
